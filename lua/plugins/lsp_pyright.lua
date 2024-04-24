b3c24af9cfffda8307a135af137205c60d2af4dc
maxcapobianco <max.capobianco1996@gmail.com>
2024-04-24 12:36:58 +1000

    Working python debugger.

--- lua/plugins/lsp_pyright.lua
+++ lua/plugins/lsp_pyright.lua
@@ -1,10 +1,46 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        pyright = {},
-      },
-    },
-  },
+  --
+  --  {
+  --    "neovim/nvim-lspconfig",
+  --    opts = {
+  --      servers = {
+  --        pyright = {
+  --          enabled = lsp == "pyright",
+  --        },
+  --        basedpyright = {
+  --          enabled = lsp == "basedpyright",
+  --        },
+  --        [lsp] = {
+  --          enabled = true,
+  --        },
+  --        ruff = {
+  --          keys = {
+  --            {
+  --              "<leader>co",
+  --              function()
+  --                vim.lsp.buf.code_action({
+  --                  apply = true,
+  --                  context = {
+  --                    only = { "source.organizeImports" },
+  --                    diagnostics = {},
+  --                  },
+  --                })
+  --              end,
+  --              desc = "Organize Imports",
+  --            },
+  --          },
+  --        },
+  --      },
+  --      setup = {
+  --        ruff = function()
+  --          LazyVim.lsp.on_attach(function(client, _)
+  --            if client.name == "ruff" then
+  --              -- Disable hover in favor of Pyright
+  --              client.server_capabilities.hoverProvider = false
+  --            end
+  --          end)
+  --        end,
+  --      },
+  --    },
+  --  },
 }
